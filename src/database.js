2c0ba688274df8160755b290f8b487af12c36dd8
Felipe <felipe8297@gmail.com>
2024-03-11 20:17:03 -0300

    feature: separating routes

--- src/database.js
+++ src/database.js
@@ -1,18 +1,40 @@
-export class DataBase {
-  database = {}
+import fs from 'node:fs/promises'
+
+const databasePath = new URL('../db.json', import.meta.url)
+
+// sempre criar sem ser camelCase - Database e não DataBase
+export class Database {
+  // ao adicionar o # na frente da variável, ela se torna um propriedade privada
+
+  #database = {}
+  constructor() {
+    fs.readFile(databasePath, 'utf-8')
+      .then((data) => {
+        this.#database = JSON.parse(data)
+      })
+      .catch(() => {
+        this.#persist()
+      })
+  }
+
+  #persist() {
+    fs.writeFile(databasePath, JSON.stringify(this.#database))
+  }
 
   select(table) {
-    const data = this.database[table] ?? []
+    const data = this.#database[table] ?? []
 
     return data
   }
 
   insert(table, data) {
-    if (Array.isArray(this.database[table])) {
-      this.database[table].push(data)
+    if (Array.isArray(this.#database[table])) {
+      this.#database[table].push(data)
     } else {
-      this.database[table] = [data]
+      this.#database[table] = [data]
     }
+
+    this.#persist()
 
     return data
   }
